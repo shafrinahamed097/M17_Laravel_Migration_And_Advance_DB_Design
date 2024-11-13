5fcf1acda4c75b23a3ec5ebeb504dc7d789e7f36
shafrinahamed97 <shafrinahamedjob@gmail.com>
2024-11-13 18:39:29 +0600

    Migration - Working With Column Types

---
+++ database/migrations/2024_11_13_104931_create_profiles.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('profiles', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->boolean('is_bangladeshi');
+            $table->bigInteger('vote');
+            $table->binary('photo');
+            $table->char('name', 50);
+            $table->dateTime('voting_date_time');
+            $table->date('voting_date');
+            $table->double('population');
+            $table->enum('group',['A', 'B']);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('profiles');
+    }
+};
